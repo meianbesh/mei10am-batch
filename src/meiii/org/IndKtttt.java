04aa50de0dc65c2a981f0a45aff85c4a61ce667d
meianbesh <meianbesh@gmail.com>
2021-09-08 16:19:29 +0530

    password

--- src/meiii/org/IndKtttt.java
+++ src/meiii/org/IndKtttt.java
@@ -1,12 +1,16 @@
 package meiii.org;
 
 public class IndKtttt {
-public void username() {
-	System.out.println("username");
-}
-public void password() {
-	System.out.println("lastname");
+	public void password() {
+		System.out.println("username");
+	}
+	public void pass() {
+		System.out.println("lastname");
+
+		System.out.println("indiab");
+
+	}
 
 }
 
-}
+
